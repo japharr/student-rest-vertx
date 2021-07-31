23e1514e596dc48165ecf671a4849f6c9fb599a0
Jelil Adesina <jtutoff@gmail.com>
2021-07-31 22:31:52 +0100

    initial commit

---
+++ src/main/java/com/japharr/App.java
@@ -0,0 +1,13 @@
+package com.japharr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
