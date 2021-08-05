2ac12933c8acd0ab880663ba2217d6ff40951ec1
Jelil Adesina <jtutoff@gmail.com>
2021-08-05 06:20:24 +0100

    move pool to mainVerticle

--- src/main/java/com/japharr/studentrest/MainVerticle.java
+++ src/main/java/com/japharr/studentrest/MainVerticle.java
@@ -1,5 +1,7 @@
 package com.japharr.studentrest;
 
+import com.japharr.studentrest.entity.Student;
+import com.japharr.studentrest.entity.StudentParametersMapper;
 import com.japharr.studentrest.handler.StudentRouterHandler;
 import com.japharr.studentrest.service.StudentService;
 import io.vertx.core.AbstractVerticle;
@@ -9,15 +11,26 @@ import io.vertx.core.Promise;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.pgclient.PgConnectOptions;
 import io.vertx.sqlclient.Pool;
+import io.vertx.sqlclient.PoolOptions;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.templates.SqlTemplate;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.japharr.studentrest.service.StudentService.*;
 
 public class MainVerticle extends AbstractVerticle {
+    private Pool pool;
     private StudentService studentService;
 
     @Override
     public void stop(Promise<Void> stopPromise) throws Exception {
-        if(studentService != null) {
-            studentService.close(r ->  {
+        if(pool != null) {
+            pool.close(r ->  {
                 if(r.succeeded()) {
                     stopPromise.complete();
                 } else {
@@ -29,14 +42,41 @@ public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start(Promise<Void> startPromise) throws Exception {
-        studentService = StudentService.init(vertx);
+        PgConnectOptions options = new PgConnectOptions()
+            .setPort(5432)
+            .setHost("localhost")
+            .setDatabase("vertx_test")
+            .setUser("devuser")
+            .setPassword("pass123");
 
-        studentService.initData((r) -> {
+        this.pool = Pool.pool(vertx, options, new PoolOptions().setMaxSize(4));
+
+        studentService = StudentService.init(pool);
+
+        initData((r) -> {
             startWebApp((http) ->
                 completeStartup(http, startPromise)
             );
         });
     }
+
+    public void initData(Handler<AsyncResult<RowSet<Row>>> handler) {
+        SqlTemplate<Map<String, Object>, RowSet<Row>> insertTemplate = SqlTemplate
+            .forQuery(pool, INSERT_TABLE_SQL);
+
+        List<Student> testList = List.of(new Student("John", "Thomas", 20, "Computer Science")
+            , new Student( "Harry", "Porter", 18, "Statistics"));
+
+        pool.query(DROP_TABLE_SQL).execute()
+            .compose(r -> pool.query(CREATE_TABLE_SQL)
+                .execute())
+            .compose(r -> insertTemplate
+                .mapFrom(StudentParametersMapper.INSTANCE)
+                .executeBatch(testList))
+            .onComplete(handler)
+            .onFailure(Throwable::printStackTrace);
+    }
+
 
     private void completeStartup(AsyncResult<HttpServer> http, Promise<Void> startPromise) {
         if (http.succeeded()) {
